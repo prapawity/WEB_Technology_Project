a09fe05b71e2e9d1d41828c466a4cec41991b0fd
Pannita Hamego <60070061@kmitl.ac.th>
2018-11-07 17:44:30 +0700

    add back to top

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+
+//Back to Top
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
 // AOS
 AOS.init({
   duration: 1000,
